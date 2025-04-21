f6f0b1fbe14b66f74f529571be4aa4119904617f
Emerson Grier <erg23282@huskies.bloomu.edu>
2025-04-21 16:10:49 -0400

    change https handdling

--- BayWave/src/main/java/com/BayWave/RequestServer.java
+++ BayWave/src/main/java/com/BayWave/RequestServer.java
@@ -6,7 +6,6 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.*;
 import java.net.InetSocketAddress;
 
-import com.BayWave.SongHandler;
 import com.sun.net.httpserver.HttpsServer;
 import com.sun.net.httpserver.HttpsConfigurator;
 
@@ -47,19 +46,11 @@ public class RequestServer {
             sslContext.init(kmf.getKeyManagers(), null, null);
 
             server.setHttpsConfigurator(new HttpsConfigurator(sslContext));
-        } catch (RuntimeException e) {
-            throw new RuntimeException(e);
-        } catch (CertificateException e) {
-            throw new RuntimeException(e);
-        } catch (KeyStoreException e) {
-            throw new RuntimeException(e);
-        } catch (NoSuchAlgorithmException e) {
-            throw new RuntimeException(e);
-        } catch (UnrecoverableKeyException e) {
-            throw new RuntimeException(e);
-        } catch (KeyManagementException e) {
-            throw new RuntimeException(e);
+        } catch (Exception e) {
+            e.printStackTrace();
+            throw new RuntimeException("Failed to set up HTTPS", e);
         }
+
 
 
         //creates contexts that will then be handled by class handler designed for that context
