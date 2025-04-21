d04fd4f050ad7cfefbf27b488392f793734c0dde
Emerson Grier <erg23282@huskies.bloomu.edu>
2025-04-21 15:50:14 -0400

    https work

--- BayWave/src/main/java/com/BayWave/RequestServer.java
+++ BayWave/src/main/java/com/BayWave/RequestServer.java
@@ -6,6 +6,17 @@ import com.sun.net.httpserver.HttpServer;
 import java.io.*;
 import java.net.InetSocketAddress;
 
+import com.BayWave.SongHandler;
+import com.sun.net.httpserver.HttpsServer;
+import com.sun.net.httpserver.HttpsConfigurator;
+
+import javax.net.ssl.KeyManagerFactory;
+import javax.net.ssl.SSLContext;
+import java.io.*;
+import java.net.InetSocketAddress;
+import java.security.*;
+import java.security.cert.CertificateException;
+
 public class RequestServer {
 
     /**
@@ -14,7 +25,42 @@ public class RequestServer {
      * @throws IOException
      */
     public static void main(String[] args) throws IOException {
-        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+        HttpsServer server = HttpsServer.create(new InetSocketAddress(8080), 0);
+
+        // Load SSL certificate
+        String passwordEnv = System.getenv("KEYSTORE_PASSWORD");
+        if (passwordEnv == null) {
+            throw new IllegalStateException("Environment variable KEYSTORE_PASSWORD not set");
+        }
+        char[] password = passwordEnv.toCharArray();
+
+
+        try {
+            KeyStore ks = KeyStore.getInstance("PKCS12");
+            FileInputStream fis = new FileInputStream("/home/developer/keystore.p12");
+            ks.load(fis, password);
+
+            KeyManagerFactory kmf = KeyManagerFactory.getInstance("SunX509");
+            kmf.init(ks, password);
+
+            SSLContext sslContext = SSLContext.getInstance("TLS");
+            sslContext.init(kmf.getKeyManagers(), null, null);
+
+            server.setHttpsConfigurator(new HttpsConfigurator(sslContext));
+        } catch (RuntimeException e) {
+            throw new RuntimeException(e);
+        } catch (CertificateException e) {
+            throw new RuntimeException(e);
+        } catch (KeyStoreException e) {
+            throw new RuntimeException(e);
+        } catch (NoSuchAlgorithmException e) {
+            throw new RuntimeException(e);
+        } catch (UnrecoverableKeyException e) {
+            throw new RuntimeException(e);
+        } catch (KeyManagementException e) {
+            throw new RuntimeException(e);
+        }
+
 
         //creates contexts that will then be handled by class handler designed for that context
 
@@ -22,7 +68,7 @@ public class RequestServer {
         server.createContext("/song", new SongHandler());
         server.createContext("/song/metadata", new SongDataHandler());
         server.createContext("/search", new SearchHandler());
-            server.createContext("/ambience", new AmbienceHandler());
+        server.createContext("/ambience", new AmbienceHandler());
         server.createContext("/playlist", new PlaylistHandler());
 
         //test to push to server
@@ -31,6 +77,6 @@ public class RequestServer {
 
         server.setExecutor(null);
         server.start();
-        System.out.println("Server started on http://localhost:8080");
+        System.out.println("Server started on https://localhost:8080");
     }
 }
