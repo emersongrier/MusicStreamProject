351b3d8560007a3da38548ad09764abc21048d95
Bailey BFH <baileypaigeinman@gmail.com>
2025-05-02 11:10:00 -0400

    Fixed InitializeTriggers.java

--- BayWave/src/main/java/com/BayWave/InitializeTriggers.java
+++ BayWave/src/main/java/com/BayWave/InitializeTriggers.java
@@ -4,7 +4,7 @@ import java.sql.*;
 
 public class InitializeTriggers {
     public static void main(String[] args) {
-        try (Connection connection = DriverManager.getConnection("jdbc:h2:~/test;INIT=RUNSCRIPT FROM 'classpath:InitializeTriggers.sql';AUTOCOMMIT=OFF;")) { // :~/test
+        try (Connection connection = DriverManager.getConnection("jdbc:h2:~/test;INIT=RUNSCRIPT FROM 'classpath:LikeTriggers.sql';AUTOCOMMIT=OFF;")) { // :~/test
             System.out.println("connection.isValid(0): " + connection.isValid(0));
             if (connection.isValid(0)) {
                 System.out.println("InitializeTriggers.sql was run successfully");
