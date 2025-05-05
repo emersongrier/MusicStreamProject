16a4991db690c93d0c02aeba354974079b9a8f8a
Bailey BFH <baileypaigeinman@gmail.com>
2025-05-05 11:53:11 -0400

    Created PlaylistSongsHandler

---
+++ BayWave/src/main/java/com/BayWave/PlaylistSongsHandler.java
@@ -0,0 +1,81 @@
+package com.BayWave;
+
+import com.BayWave.Tables.PlaylistTrackTable;
+import com.google.gson.Gson;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Map;
+
+import static com.BayWave.ParseQuery.parseQuery;
+import static com.BayWave.Util.ServerUtil.getConnection;
+
+
+class PlaylistSongsHandler implements HttpHandler
+{
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+        if (!"GET".equals(exchange.getRequestMethod())) {
+            exchange.sendResponseHeaders(405, -1);
+            return;
+        }
+        String query = exchange.getRequestURI().getQuery();
+        Map<String, String> params = parseQuery(query);
+        if (params == null) {
+            exchange.sendResponseHeaders(400, -1);
+            return;
+        }
+        String id = params.get("id");
+
+        if (id == null) {
+            exchange.sendResponseHeaders(400, -1);
+            return;
+        }
+
+        // Establish database connection. Replace with your connection details.
+        Connection connection;
+        try {
+            connection = getConnection();
+        } catch (SQLException e) {
+            System.err.println("Unable to establish DB connection: " + e.getMessage());
+            return;
+        }
+
+        String[] playlistsongs;
+
+        try {
+            playlistsongs = PlaylistTrackTable.getTracks(connection, Integer.parseInt(id));
+        } catch (SQLException e) {
+            System.err.println("Could not get user info: " + e.getMessage());
+            return;
+        }
+
+        if (playlistsongs == null) {
+            exchange.sendResponseHeaders(404, -1);
+            return;
+        }
+
+        Gson gson = new Gson();
+        String json = gson.toJson(playlistsongs);
+
+        byte[] responseBytes = json.getBytes(StandardCharsets.UTF_8);
+
+        exchange.getResponseHeaders().set("Content-Type", "application/json");
+        exchange.sendResponseHeaders(200, responseBytes.length);
+
+
+        try (OutputStream os = exchange.getResponseBody()) {
+            os.write(responseBytes);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
--- BayWave/src/main/java/com/BayWave/RequestServer.java
+++ BayWave/src/main/java/com/BayWave/RequestServer.java
@@ -65,6 +65,7 @@ public class RequestServer {
         server.createContext("/playlist/create", new CreatePlaylistHandler());
         server.createContext("/playlist/addsong",new AddSongPlaylistHandler());
         server.createContext("/playlist/deletesong", new DeleteSongPlaylistHandler());
+        server.createContext("/playlist/songs", new PlaylistSongsHandler());
         server.createContext("/artist/metadata", new ArtistDataHandler());
         server.createContext("/album/metadata", new AlbumDataHandler());
 
